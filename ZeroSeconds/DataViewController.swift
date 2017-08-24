4bee02b53072584c6fd48b3e29db8dcfd8c3fdcb
Elliot Barer <ebarer@mac.com>
2017-08-24 00:16:31 -0700

    Initial Commit

---
+++ ZeroSeconds/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  ZeroSeconds
+//
+//  Created by Elliot Barer on 2017-08-24.
+//  Copyright © 2017 ebarer. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
